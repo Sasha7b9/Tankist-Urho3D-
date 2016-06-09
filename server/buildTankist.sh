c80261846e301f72d3080d25a211fa2acb94e0dd
root <root@tankistmk.ru>
2016-06-09 09:37:55 +0300

    Modify buildTankist.sh script

--- server/buildTankist.sh
+++ server/buildTankist.sh
@@ -3,8 +3,13 @@
 rm -r build
 mkdir build
 cd build
+cp -r ../../out/distr/* .
 cp ../../../urho3D/build/lib/libUrho3D.a .
-cp -r ../../../urho3D/bin/CoreData .
-cp -r ../../../urho3D/bin/Data .
 cmake ..
 make -j2
+rm *.*
+rm -r CMakeFiles
+rm Makefile
+cp ../../../../../Downloads/* .
+cp ../../scripts/list_files.py .
+python3 list_files.py

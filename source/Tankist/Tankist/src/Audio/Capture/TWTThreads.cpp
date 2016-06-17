4508596cd389518f92ea837229a5edba4204239d
Sasha <Sasha7b9@tut.by>
2016-06-17 06:57:48 +0300

    Adding files for capture and playing files.

---
+++ source/Tankist/Tankist/src/Audio/Capture/TWTThreads.cpp
@@ -0,0 +1,13 @@
+#include "TWTThreads.h"
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+Signal::Signal(int count)
+{
+    signalCount = count;
+
+    for (int a = 0; a < count; a++)
+    {
+        signalEvent[a] = CreateEventA(nullptr, false, false, nullptr);
+    }
+}

f161fa79f0b2a2a14db309543b10048d2ffe3bf2
Sasha <Sasha7b9@tut.by>
2017-09-26 23:20:06 +0300

    Рефакторинг

--- source/Tankist/Tankist/src/Network/Client.h
+++ source/Tankist/Tankist/src/Network/Client.h
@@ -17,26 +17,34 @@ public:
     ~Client();
 
     bool ConnectToServer();
-    void Disconnect();
-    bool AttachCameraToNode();
 
-    // Request ping, load cpu, number clients
+    void Disconnect();
+
+    bool AttachCameraToNode();
+    /// Запрос пинга, загрузки ЩПУ, числа клиентов
     void RequestSystemInformation();
 
     void MessageControl(Control control, TypeControl type);
 
     unsigned towerID = 0;
+
     unsigned trunkID = 0;
+
     unsigned caseID = 0;
 
 private:
+
     void HandleStringMessage(StringHash, VariantMap&);
+
     void HandleClientObjectID(StringHash, VariantMap &);
+
     void HandleServerConnected(StringHash, VariantMap &);
+
     void HandleServerDisconnected(StringHash, VariantMap &);
+
     void HandleConnectFailed(StringHash, VariantMap &);
+
     void HandleNetworkMessage(StringHash, VariantMap &);
-    void HandleClientConnected(StringHash, VariantMap &);
 
     bool serverIsConnected = false;
 
